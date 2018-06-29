6dd772d75bb0d0321a01f3c5178cb3439e77ea90
pdqnguyen <philnguyen94@gmail.com>
2018-06-28 17:43:16 -0700

    renamed some scripts

--- scrape_darklyrics.py
+++ scrape_darklyrics.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 BASEURL = 'http://www.darklyrics.com'
 
@@ -16,12 +17,12 @@ def get_album_lyrics(album_name, album_url):
                 s = str(line).replace('\n', '').replace('"', '')
                 if s != '':
                     song_lyrics.append(s)
-            elif line.name != 'br':
+            elif line.name != 'br' and line.name != 'i':
                 break
         album_lyrics[song_name] = song_lyrics
     return album_lyrics
 
-def get_band_lyrics(band_name):
+def get_band_lyrics(band_name, verbose=False):
     band_url = BASEURL + '/' + band_name[0] + '/' + band_name + '.html'
     with urlopen(band_url) as band_page:
         soup = BeautifulSoup(band_page, 'html.parser')
@@ -29,9 +30,12 @@ def get_band_lyrics(band_name):
     band_lyrics = {}
     for album in albums_html:
         album_name = album.find('strong').text.replace('"', '')
-        album_url = album.find('a', href=True)['href'][2:-2]
+        album_url = BASEURL + album.find('a', href=True)['href'][2:-2]
+        if verbose:
+            print(album_name, album_url)
         album_lyrics = get_album_lyrics(album_name, album_url)
         band_lyrics[album_name] = album_lyrics
+    return band_lyrics
 
 if __name__ == '__main__':
     import sys
