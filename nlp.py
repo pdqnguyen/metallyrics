98174fa7b256f4b76c8263018f03553becaf1b06
pdqnguyen <philnguyen94@gmail.com>
2020-03-06 22:08:08 -0800

    added tokenize function

--- nlp.py
+++ nlp.py
@@ -2,8 +2,11 @@ import glob
 import os
 import re
 
+from nltk.corpus import words as nltk_words
+
 
 STOPWORDS_DIR = os.path.abspath('stopwords')
+ENGLISH_WORDS = set(nltk_words.words())
 
 
 def get_stopwords(stopword_dir=STOPWORDS_DIR):
@@ -16,3 +19,16 @@ def get_stopwords(stopword_dir=STOPWORDS_DIR):
             if re.match('^\w+$', word):
                 stopwords.add(word)
     return stopwords
+
+
+def tokenize(s, lower=True, alpha_only=True, english_only=True, stopwords=None):
+    words = s.split()
+    if lower:
+        words = [word.lower() for word in words]
+    if alpha_only:
+        word = [word for word in words if word.isalpha()]
+    if english_only:
+        words = [word for word in words if word in ENGLISH_WORDS]
+    if stopwords:
+        words = [word for word in words if word.lower() not in stopwords]
+    return words
