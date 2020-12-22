917dc49f2d2c0156bc5d9a49aca02614dc3a16cb
pdqnguyen <philnguyen94@gmail.com>
2020-12-22 13:44:41 -0800

    custom Pipeline object

--- nlp.py
+++ nlp.py
@@ -2,6 +2,8 @@ import glob
 import os
 import re
 
+import scipy
+
 from nltk.corpus import words as nltk_words
 
 
@@ -32,3 +34,57 @@ def tokenize(s, lower=True, alpha_only=True, english_only=True, stopwords=None):
     if stopwords:
         words = [word for word in words if word.lower() not in stopwords]
     return words
+
+
+class Pipeline:
+    """Pipeline for NLP classification with vectorization and resampling
+
+    Parameters
+    ----------
+    vectorizer : transformer object
+        Object with `fit_transform` and `transform` methods for vectorizing
+        corpus data.
+
+    resampler : resampler object
+        Object with fit_resample method for resampling training data in
+        `Pipeline.fit`. This can be any under/oversampler from `imblearn`
+        for binary or multiclass classification, or `MLSOL` from
+        https://github.com/diliadis/mlsol/blob/master/MLSOL.py for multi-label
+        classification.
+
+    classifier : estimator object
+        Binary, multi-class, or multi-label classifier with a `predict`
+        or `predict_proba` method.
+
+    Methods
+    -------
+    fit(X, y)
+        Fit vectorizer and resampler, then train classifier on transformed data.
+
+    predict(X)
+        Return classification probabilities (if `self.classifier` has a
+        `predict_proba` method, otherwise return predictions using `predict`).
+    """
+    def __init__(self, vectorizer, resampler, classifier):
+        self.vectorizer = vectorizer
+        self.resampler = resampler
+        self.classifier = classifier
+
+    def fit(self, X, y):
+        X_v = self.vectorizer.fit_transform(X)
+        X_r, y_r = self.resampler.fit_resample(X_v.toarray(), y)
+        self.classifier.fit(X_r, y_r)
+        return self
+
+    def predict(self, X):
+        X_v = self.vectorizer.transform(X)
+        try:
+            y_p = self.classifier.predict_proba(X_v)
+        except AttributeError:
+            y_p = self.classifier.predict(X_v)
+        if (
+                isinstance(y_p, scipy.sparse.csr.csr_matrix) or
+                isinstance(y_p, scipy.sparse.lil.lil_matrix)
+        ):
+            y_p = y_p.toarray()
+        return y_p
